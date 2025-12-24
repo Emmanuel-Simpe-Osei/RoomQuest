514d7ceeb2970125245f9332d8a76492cdc64e00
Emmanuel-Simpe-Osei <emmanuelsimpeosei5@gmail.com>
2025-12-24 12:49:01 +0000

    add site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://roomquestaccomodations.com", // your live domain
-  generateRobotsTxt: true, // generates robots.txt
-  sitemapSize: 5000, // optional: splits sitemap if many pages
+  siteUrl: "https://www.roomquestaccomodations.com",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
 };
